15c21f14b7254e15262c5d94ffd9842fcdde04b3
Adam Hartman <adamhartman5@gmail.com>
2023-03-12 01:31:29 -0500

    Create side nav

--- app/SideNavigation.tsx
+++ app/SideNavigation.tsx
@@ -1,9 +1,69 @@
+'use client';
+
 import React from 'react';
+import { IconType } from 'react-icons';
+import { HiHome } from 'react-icons/hi';
+import { TfiYoutube } from 'react-icons/tfi';
+import { usePathname, useRouter } from 'next/navigation';
+import { BsCollectionPlay } from 'react-icons/bs';
+import { AiOutlineYoutube } from 'react-icons/ai';
+import { BiPlayCircle } from 'react-icons/bi';
 
-type Props = {};
+interface MenuItem {
+	icon: IconType;
+	text: string;
+	pathname: string;
+}
 
-const SideNavigation = (props: Props) => {
-	return <div className='border-2 border-orange-300'>SIDE NAVIGATION</div>;
+const MenuItems: Array<MenuItem> = [
+	{
+		icon: HiHome,
+		text: 'Home',
+		pathname: '/',
+	},
+	{
+		icon: TfiYoutube,
+		text: 'Shorts',
+		pathname: '/shorts',
+	},
+	{
+		icon: BsCollectionPlay,
+		text: 'Subscriptions',
+		pathname: '/subscriptions',
+	},
+	{
+		icon: AiOutlineYoutube,
+		text: 'Originals',
+		pathname: '/originals',
+	},
+	{
+		icon: BiPlayCircle,
+		text: 'YouTube Music',
+		pathname: '/music',
+	},
+];
+
+const SideNavigation = () => {
+	const pathname = usePathname();
+	const router = useRouter();
+	const handleItemClick = (item: MenuItem) => {
+		router.push(item.pathname);
+	};
+	return (
+		<div className='p-[4px] lg:px-3 lg:w-[260px]'>
+			{MenuItems.map((item) => (
+				<div
+					className={`flex flex-col lg:flex-row lg:text-base lg:gap-6 gap-1 p-4 lg:py-2 items-center hover:bg-zinc-700 ${
+						item.pathname === pathname && 'bg-zinc-700 hover:bg-zinc-600'
+					} rounded-lg cursor-pointer`}
+					onClick={() => handleItemClick(item)}
+				>
+					<item.icon size={30} />
+					<span className='text-xs'>{item.text}</span>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default SideNavigation;
---
+++ app/shorts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+	return (
+		<div>Shorts page</div>
+	)
+}
+
+export default page
